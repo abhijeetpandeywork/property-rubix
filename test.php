f0930e4c709a718fcce5763ab424d17552a0e1f6
abhijeetpandeywork <abhijeetpandeywork@users.noreply.github.com>
2026-07-30 11:59:51 +0530

    update test for localities

--- test.php
+++ test.php
@@ -1,12 +1,7 @@
 <?php
 require "config/db.php";
 $pdo = db();
-$stmt = $pdo->query("
-    SELECT p.id, p.name, p.city_id, p.locality_id, p.location_area, c.name as city_name 
-    FROM projects p 
-    JOIN cities c ON c.id = p.city_id 
-    WHERE c.name = 'Mumbai'
-");
-echo "<pre>Projects in Mumbai:\n";
+$stmt = $pdo->query("SELECT id, name, slug FROM localities WHERE city_id=5");
+echo "<pre>Localities for Mumbai (id=5):\n";
 print_r($stmt->fetchAll(PDO::FETCH_ASSOC));
 echo "</pre>";
