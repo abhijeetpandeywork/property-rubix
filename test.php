ea02df0e30099ec38c12a87c529fb67122940843
abhijeetpandeywork <abhijeetpandeywork@users.noreply.github.com>
2026-08-10 12:48:44 +0530

    debug: check columns

--- test.php
+++ test.php
@@ -1,7 +1,7 @@
 <?php
 require "config/db.php";
 $pdo = db();
-$stmt = $pdo->query("SELECT id, name, slug FROM localities WHERE city_id=5");
-echo "<pre>Localities for Mumbai (id=5):\n";
+$stmt = $pdo->query("SHOW COLUMNS FROM projects");
+echo "<pre>";
 print_r($stmt->fetchAll(PDO::FETCH_ASSOC));
 echo "</pre>";
